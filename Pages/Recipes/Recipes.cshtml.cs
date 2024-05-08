68cb3a9947892d4e754495ecc190b4178f51043b
Paige Winnett <winnettpaige2@gmail.com>
2024-05-07 22:59:43 -0500

    save

--- Pages/Recipes/Recipes.cshtml.cs
+++ Pages/Recipes/Recipes.cshtml.cs
@@ -2,6 +2,7 @@ using FinalProject.models;
 using FinalProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FinalProject.Pages;
 
@@ -10,6 +11,12 @@ public class RecipeModel : PageModel
     private readonly RecipeContext _context; 
     private readonly ILogger<IndexModel> _logger;
     public List<Recipe> Recipes {get; set;} = default!;
+    public Recipe FindRecipe {get; set;} = default!;
+    public string RecipeId {get; set;} = string.Empty;
+    public SelectList RecipeDropDown {get; set;} = default!;
+    [BindProperty]
+    public Recipe Recipe {get; set;} = default!;
+
 
 
     public RecipeModel(RecipeContext context,ILogger<IndexModel> logger)
@@ -21,5 +28,18 @@ public class RecipeModel : PageModel
     public void OnGet()
     {
         Recipes = _context.Recipes.ToList();
+        FindRecipe = _context.Recipes.Find(RecipeId)!;
+        RecipeDropDown = new SelectList(Recipes, "RecipeId", "Title");
     }
+    public IActionResult OnPost()
+{
+    if (!ModelState.IsValid)
+    {
+        return Page();
+    }
+    _context.Recipes.Add(Recipe);
+    _context.SaveChanges();
+    return RedirectToPage("./Index");
+}
+
 }
